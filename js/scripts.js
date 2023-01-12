dd1c69e169a69db14014fee4eac7f5244786aa63
Vera weikel <vera.weikel@gmail.com>
2023-01-12 13:33:50 -0800

    It will update js with calculator fx and add fx..
    Co-authored-by: Justin Lee<lee.justin001126@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,13 @@
+function add(number1, number2){ 
+  return number1 + number2;
+};
 
-// window.addEventListener("load", function() {
-//   const form = document.querySelector("form#calculator");
-// //   form.addEventListener("submit", eventHandleCalculationSubmit); 
-// // });
+function calculator(number1, number2, operator){
+  if (operator === "+") {
+    return number1 + number2;
+  }
+};
 
-// function eventHandleCalculationSubmit(event){
-//   event.preventDefault();
-//   const number1 = parseInt(document.getElementById("number-1-id").value);
-//   const number2 = parseInt(document.getElementById("number-2-id").value);
-
-
-// console.log("Num1&num2: ", number1, number2);
-
-//   // result
-// let nums = "Display numbers" +  number1 + " and " + number2;
-
-// document.getElementById("#result)").innerText = nums;
-
-// }
 
 // User Interface Logic
 function handleCalculation(event) {
@@ -28,13 +18,12 @@ function handleCalculation(event) {
   const number2 = parseInt(document.getElementById("number-2-id").value);
   const operator = document.getElementById("operator").value;
 
-
-console.log("Num1&num2: ", number1, number2);
+  const answer = calculator(number1, number2, operator);
 
   // result
-let nums = "Display numbers: " +  number1 + " " + operator + " " + number2;
+  let nums = "Display numbers: " +  number1 + " " + operator + " " + number2 + " " + answer;
 
-document.getElementById("result").innerText = nums;
+  document.getElementById("result").innerText = nums;
 
 }
 
