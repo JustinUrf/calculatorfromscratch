52cac51818034c011b43e98c342fa159a98e0a6d
Vera weikel <vera.weikel@gmail.com>
2023-01-12 11:53:54 -0800

    It will update event and form intake for 2 nums on js.
    Co-authored-by: Justin Lee<lee.justin001126@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+// window.addEventListener("load", function() {
+//   const form = document.querySelector("form#calculator");
+// //   form.addEventListener("submit", eventHandleCalculationSubmit); 
+// // });
+
+// function eventHandleCalculationSubmit(event){
+//   event.preventDefault();
+//   const number1 = parseInt(document.getElementById("number-1-id").value);
+//   const number2 = parseInt(document.getElementById("number-2-id").value);
+
+
+// console.log("Num1&num2: ", number1, number2);
+
+//   // result
+// let nums = "Display numbers" +  number1 + " and " + number2;
+
+// document.getElementById("#result)").innerText = nums;
+
+// }
+
+// User Interface Logic
+function handleCalculation(event) {
+  event.preventDefault();
+  // the code to get and process form values will go here!
+
+  const number1 = parseInt(document.getElementById("number-1-id").value);
+  const number2 = parseInt(document.getElementById("number-2-id").value);
+
+
+console.log("Num1&num2: ", number1, number2);
+
+  // result
+let nums = "Display numbers" +  number1 + " and " + number2;
+
+document.getElementById("result").innerText = nums;
+
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", handleCalculation);
+});
