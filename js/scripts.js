90844baa418582a3a69419f389c3540126178c7b
Vera weikel <vera.weikel@gmail.com>
2023-01-12 13:49:28 -0800

    It will update js with calculator fx and mult/divide to fx..
    Co-authored-by: Justin Lee<lee.justin001126@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,24 @@ function subtract(number1, number2){
   return number1 - number2;
 }
 
+function multiply(number1, number2){
+  return number1 * number2;
+}
+
+function divide(number1, number2){
+  return number1 / number2;
+}
+
 function calculator(number1, number2, operator){
   if (operator === "+") {
     return add(number1, number2);
   } else if (operator === "-"){
     return subtract(number1, number2);
-  }
+  } else if (operator === "*") {
+    return multiply(number1, number2);
+  } else if (operator === "/") {
+    return divide(number1, number2);
+  } else ( "Select an operator");
 };
 
 
